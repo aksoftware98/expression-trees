d3aeb8b6f7224d8e87cf07216d7969218eb3e155
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-05-15 00:12:07 +0400

    Implement a basic expression

--- ExpressionTreesDemo/ExpressionTreesDemo/Program.cs
+++ ExpressionTreesDemo/ExpressionTreesDemo/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+
 Console.WriteLine("Hello, World!");
+
+// Test Delegates
+var writeMessage = WriteMessageToConsole;
+
+// basic expressoin 
+var parameterExpression = Expression.Parameter(typeof(int), "a");
+var constantExpression = Expression.Constant(12);
+var operatorExpression = Expression.GreaterThanOrEqual(parameterExpression, constantExpression);
+var functionExpression = Expression.Lambda<Func<int, bool>>(operatorExpression, parameterExpression);
+
+var combiledFunction = functionExpression.Compile();
+
+Console.WriteLine(combiledFunction(4));
+
+static void WriteMessageToConsole(string message)
+{
+    Console.WriteLine(message);
+}
+
+public delegate void WriteMessage(string message); 
+
+
